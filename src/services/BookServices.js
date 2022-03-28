73f28a99f79aa30ef0c091351292de64a74afb8c
Muhammad Acla <muhamadacla22@gmail.com>
2022-03-28 07:19:58 +0700

    added src/services

---
+++ src/services/BookServices.js
@@ -0,0 +1,34 @@
+// C.R.U.D services for https://bookshelf-api-bangkit.herokuapp.com/books url.
+
+import http from '../http-common';
+const getAll = () => {
+    return http.get('/books');
+}
+const get = (id) => {
+    return http.get(`/books/${id}`);
+}
+const create = data => {
+    return http.post("/books", data);
+  };
+const update = (id, data) => {
+    return http.put(`/books/${id}`, data);
+};
+const remove = id => {
+    return http.delete(`/books/${id}`);
+};
+const removeAll = () => {
+    return http.delete(`/books`);
+};
+const findByName = name => {
+    return http.get(`/books?name=${name}`);
+};
+
+export default {
+    getAll,
+    get,
+    create, 
+    update,
+    remove,
+    removeAll,
+    findByName
+}
