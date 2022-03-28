14ced9194d5ab1fcd05b449e8d1ce75127c423a4
Muhammad Acla <muhamadacla22@gmail.com>
2022-03-28 07:17:20 +0700

    added src/app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import AddBook from './components/AddBook';
+import Book from './components/Book';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          Bookshelf API
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/books"} className="nav-link">
+              Books
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/books"]} component={BookList} />
+          <Route exact path="/add" component={AddBook} />
+          <Route path="/books/:id" component={Book} />
+        </Switch>
+      </div>
     </div>
   );
 }
